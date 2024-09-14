423aeb04e8f4b22b45ebe0a1f891c14f020bc08c
Forthsite <produkceforthsite@email.cz>
2024-09-14 19:09:10 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import bg from './assets/bg.jpg'
+import logo from './assets/logo.png'
+import video from './assets/MeliBeauty2Final.mp4'
+
+function App() {
+  const [isPressed, setIsPressed] = useState(false);
+
+  function pressHandler() {
+    setIsPressed(true)
+  }
+
+  return (
+    <>
+      <div className='flex flex-col gap-8 items-center justify-center w-full h-full min-h-screen bg-cover' style={{backgroundImage: `url(${bg})`}}>
+        <div className='flex flex-col gap-8 items-center justify-center bg-black/35 w-full h-full min-h-screen'>
+          <div className='w-64'>
+            <img src={logo} alt="Logo" />
+          </div>
+          {isPressed ? 
+            ( <video src={video} controls width="800" height="450" className='rounded-lg' /> )
+            :
+            ( <button className='bg-slate-800 h-8 px-8 rounded-2xl uppercase font-thin text-yellow-300 transition-all hover:scale-95' onClick={pressHandler}>Spustit video Meli Beauty</button> )
+          }
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
