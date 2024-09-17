d4fc872bd7df405888bede4d6c86246a7ab156c7
Forthsite <produkceforthsite@email.cz>
2024-09-17 20:20:59 +0200

    change vid

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import bg from './assets/bg.jpg'
 import logo from './assets/logo.png'
-import video from './assets/meli2day2.mp4'
+import video from './assets/meli2day3.mp4'
 
 function App() {
   const [isPressed, setIsPressed] = useState(false);
